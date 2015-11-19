8a16945b79eae35fc233434dbaef7a8fa747c94f
Kevin Tang <ktang012@ucr.edu>
2015-11-19 02:46:01 -0800

    defined functions and added details

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Feature Selection
+CS170 - Intro to Artificial Intelligence project.
 
+Given a data set with two classes, the program will find the best features suited for classification.
+Classification will be done with the nearest neighbor algorithm and tested with one-out cross validation.
+It includes forward selection, backward elimination, and a custom search that should be more fast or optimal than the previous two.
+
+## Forward Selection
+Start with 0 features, add one at a time with a greedy search for best accuracy.
+
+## Backward Elimination
+Start with n features, remove one at a time with a greedy search for best accuracy.
+
+## Custom Search
+For my custom search, I plan to use simulated annealing paired with the use of a heuristic.
+The heuristic favors selecting the second best feature of the previous search while searching the current features.
+It may be faster and better.
+If I'm too lazy, I'll drop the heuristic which also makes it better, but maybe slower.
--- feature_selection.py
+++ feature_selection.py
@@ -1,27 +1,64 @@
 from pprint import pprint
-from sklearn import
+import random
 
 # constants for easy testing
 # instances[i][0] is class name
-my_file_name = "cs_170_small1.txt"
-my_num_instances = 100
 
-# arbitrary functions
 def LoadData(file_name, num_instances):
-    f = open(file_name, 'r')
+    try:
+        f = open(file_name, 'r')
+    except:
+        raise FileNotFoundError(file_name)
     instances = [[] for i in range(num_instances)]
     for i in range(num_instances):
         instances[i] = [float(j) for j in f.readline().split()]
-
     return instances
 
-# start here
-file_name = input("Enter the name of the file to test: ")
-num_instances = int(input("Enter the number of instances to read: "))
-instances = LoadData(my_file_name, my_num_instances)
-alg = input("""Type in the algorithm you want to run:
-               FS - Forward Selection
-               BE - Backward Elimination
-               CS - Custom Search
-            \r""")
-print("There are %d features with %d instances." % (len(instances[0]) - 1, num))
+def OneOutCrossValidation(data, current_set, feature_to_add):
+    """
+    Subject to change:
+    data is n-1 instances set with m features
+    current_set is the instance that is out for testing
+    feature_to_add is the feature to be tested
+    """
+    accuracy = random.randint(0, 100)
+    return accuracy
+
+def ForwardSelection(data, num_instances):
+    pass
+
+def BackwardElimination(data, num_instances):
+    pass
+
+def CustomSearch(data, num_instances):
+    """
+    Plan to use simulated annealing:
+    It can give better results, but might be slower
+    How to make it quicker?
+    Use heuristic that favors the second best feature of
+    the previous search when searching current features
+    Might be quicker, but is it admissible? NO CLUE
+    But since we're favoring the second best feature -- probably
+    """
+    pass
+
+def NearestNeighbor(data, current_set):
+    pass
+
+def main():
+    file_name = input("Enter the name of the file to test: ")
+    num_instances = int(input("Enter the number of instances to read: "))
+    instances = LoadData(file_name, num_instances)
+
+    alg = ""
+    while (alg != "FS" and alg != "BE" and alg != "CS"):
+        alg = input("""Type in the algorithm you want to run:
+                       FS - Forward Selection
+                       BE - Backward Elimination
+                       CS - Custom Search
+                    \r""")
+    print("There are %d features with %d instances." % (len(instances[0]) - 1, num_instances))
+    pprint(instances)
+
+if __name__ == '__main__':
+    main()
